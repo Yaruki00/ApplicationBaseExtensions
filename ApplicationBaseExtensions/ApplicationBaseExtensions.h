86d29d7a67765a40eb11ddf51b8f4c6128dffc0f
Yuta Kawabe <yuyu3165@gmail.com>
2018-01-12 15:25:53 +0900

    Initial Commit

---
+++ ApplicationBaseExtensions/ApplicationBaseExtensions.h
@@ -0,0 +1,19 @@
+//
+//  ApplicationBaseExtensions.h
+//  ApplicationBaseExtensions
+//
+//  Created by Yuta Kawabe on 2018/01/12.
+//  Copyright © 2018年 Yuta Kawabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApplicationBaseExtensions.
+FOUNDATION_EXPORT double ApplicationBaseExtensionsVersionNumber;
+
+//! Project version string for ApplicationBaseExtensions.
+FOUNDATION_EXPORT const unsigned char ApplicationBaseExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApplicationBaseExtensions/PublicHeader.h>
+
+
